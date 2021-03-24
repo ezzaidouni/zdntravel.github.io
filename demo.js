083d8d9620c27379c425a05b077f66c8bc44772e
ezzaidouni <61852572+ezzaidouni@users.noreply.github.com>
2021-03-24 12:55:26 +0100

    Update demo.js

--- demo.js
+++ demo.js
@@ -25,4 +25,5 @@ function showSlides() {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 7000); 
+  setTimeout(showSlides, 4000); 
+}
